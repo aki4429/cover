4aac70863bcdfb6f8b25cae226c9fff475d2422d
Akiyoshi Oda <akiyoshi.oda@gmail.com>
2021-08-11 00:38:40 +0000

    棚卸表作成

--- loc/make_tana.py
+++ loc/make_tana.py
@@ -2,28 +2,38 @@
 #inputs = LocData.objects.order_by('banch')
 #棚卸用に箱に添付するラベルのデータを作成する
 
-#HEADDER = ["code", "piece", "fab", "address"]
+HEADDER = ["address", "code", "sebango", "qty"]
 
 def tana(inputs, bangos):
     data = []
-    #data.append(HEADDER)
+    data.append(HEADDER)
     for row in inputs:
         #empty は除く
-        if len(row.code.split("-")) >1 :
-            line=[]
-            #item =row.code.split("-")[0].replace("013", "")
-            item =row.code.split("-")[0]
-            piece =row.code.split("-")[1].split(" ")[0]
-            fab =row.code.split("-")[1].split(" ")[1]
-            line.append(item)
-            line.append(piece)
-            line.append(fab)
-            line.append(row.banch)
-            #背番号リストとコードが一致するとき
-            for ban in bangos:
-                if row.code == ban.hcode: 
-                    line.append(ban.se)
+        #if len(row.code.split("-")) >1 :
+        line=[]
+        #item =row.code.split("-")[0].replace("013", "")
+        if row.code == 'empty':
+            orig_code = 'empty'
+        else:
+            orig_code = row.code.replace('013CH', '013').replace('013271I', '013271').replace('013232WI', '013232W')
+        #item =row.code.split("-")[0]
+        #piece =row.code.split("-")[1].split(" ")[0]
+        #fab =row.code.split("-")[1].split(" ")[1]
+        line.append(row.banch)
+        line.append(orig_code)
+        #line.append(item)
+        #line.append(piece)
+        #line.append(fab)
+        #背番号リストとコードが一致するとき
+        for ban in bangos:
+            if orig_code == 'empty':
+                line.append(" ")
+            elif orig_code == ban.hcode: 
+                line.append(ban.se)
 
-            data.append(line)
+        line.append(row.qty)
+        data.append(line)
                 
     return data
+
+
